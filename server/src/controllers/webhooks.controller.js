f82899a12695381138f20c1d3920c34bd844c5fa
saurabh kumar yadav <165639161+Saurabhkumar9@users.noreply.github.com>
2025-05-18 22:29:45 +0530

    Update webhooks.controller.js

--- server/src/controllers/webhooks.controller.js
+++ server/src/controllers/webhooks.controller.js
@@ -1,5 +1,5 @@
 
-const {Webhook} =require('svix')
+const { Webhook } =require('svix')
 const User =require('../models/user.model')
 const clerkWebhooks = async (req, res) => {
     try {
@@ -16,7 +16,7 @@ const clerkWebhooks = async (req, res) => {
             case 'user.created': {
                 const userData = {
                     _id: data.id,
-                    email: data.email_address[0].email_address,
+                    email: data.email_addresses[0].email_address,
                     name: data.first_name + " " + data.last_name,
                     imageUrl: data.image_url,
                 };
@@ -27,7 +27,7 @@ const clerkWebhooks = async (req, res) => {
 
             case 'user.updated': {
                 const userData = {
-                    email: data.email_address[0].email_address,
+                    email: data.email_addresses[0].email_address,
                     name: data.first_name + " " + data.last_name,
                     imageUrl: data.image_url,
                 };
